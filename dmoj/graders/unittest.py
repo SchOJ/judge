18cb17d6bbe14f10d724352bb6ac820dc38ea639
Outvi V <19144373+outloudvi@users.noreply.github.com>
2020-04-10 12:44:36 +0800

    fix: unittest doesn't check. =0 is enough for AC

--- dmoj/graders/unittest.py
+++ dmoj/graders/unittest.py
@@ -7,7 +7,7 @@ import os
 from dmoj.error import OutputLimitExceeded
 from dmoj.executors import executors
 from dmoj.graders.base import BaseGrader
-from dmoj.result import CheckerResult, Result
+from dmoj.result import Result
 from dmoj.judgeenv import get_problem_root
 
 log = logging.getLogger('dmoj.graders')
@@ -33,19 +33,15 @@ class UnitTestGrader(BaseGrader):
         process = self._current_proc
 
         self.populate_result(error, result, process)
-
-        check = self.check_result(case, result)
-
-        # checkers must either return a boolean (True: full points, False: 0 points)
-        # or a CheckerResult, so convert to CheckerResult if it returned bool
-        if not isinstance(check, CheckerResult):
-            check = CheckerResult(check, case.points if check else 0.0)
-
+        
         if result.result_flag & Result.IR:
             result.result_flag = Result.WA
-        result.points = check.points
-        result.feedback = check.feedback or result.feedback
-        result.extended_feedback = check.extended_feedback or result.extended_feedback
+        if result.result_flag == Result.AC:
+            result.points = case.points
+        else:
+            result.points = case.points
+        result.feedback = result.feedback
+        result.extended_feedback = result.extended_feedback
 
         case.free_data()
 
@@ -63,36 +59,6 @@ class UnitTestGrader(BaseGrader):
 
     def populate_result(self, error, result, process):
         self.binary.populate_result(error, result, process)
-
-    def check_result(self, case, result):
-        # If the submission didn't crash and didn't time out, there's a chance it might be AC
-        # We shouldn't run checkers if the submission is already known to be incorrect, because some checkers
-        # might be very computationally expensive.
-        # See https://github.com/DMOJ/judge/issues/170
-        checker = case.checker()
-        # checker is a `partial` object, NOT a `function` object
-        if not result.result_flag or getattr(checker.func, 'run_on_error', False):
-            try:
-                check = checker(result.proc_output,
-                                case.output_data(),
-                                submission_source=self.source,
-                                judge_input=case.input_data(),
-                                point_value=case.points,
-                                case_position=case.position,
-                                batch=case.batch,
-                                submission_language=self.language,
-                                binary_data=case.has_binary_data,
-                                execution_time=result.execution_time,
-                                problem_id=self.problem.id)
-            except UnicodeDecodeError:
-                # Don't rely on problemsetters to do sane things when it comes to Unicode handling, so
-                # just proactively swallow all Unicode-related checker errors.
-                return CheckerResult(False, 0, feedback='invalid unicode')
-        else:
-            # Solution is guaranteed to receive 0 points
-            check = False
-
-        return check
 
     def _launch_process(self, case):
         self._current_proc = self.binary.launch(
